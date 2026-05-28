75bbc751320243a2efe3b4a2168383a0cfa7a0f3
oguevara-code <oguevara@unsa.edu.pe>
2026-05-28 17:47:51 -0500

    Creación de modelos Alumno Curso y NotaAlumnoPorCurso

--- sistema_notas/academico/models.py
+++ sistema_notas/academico/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+class Alumnos(models.Model):
+    codigo = models.CharField(max_length=10, unique=True)
+    nombre = models.CharField(max_length=100)
+    correo = models.EmailField()
+
+    def __str__(self):
+        return self.nombre
+    
+
+class Curso(models.Model):
+    codigo = models.CharField(max_length=10, unique=True)
+    nombre = models.CharField(max_length=100)
+    creditos = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.nombre
+    
+
+class NotaAlumnoPorCurso(models.Model):
+    alumno = models.ForeignKey(Alumnos, on_delete=models.CASCADE)
+    curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+    nota = models.DecimalField(max_digits=4, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.alumno.nombre} - {self.curso.nombre}"
