9b40f0dada7a8ca10f5235daed311f5aaf927ba6
oguevara-code <oguevara@unsa.edu.pe>
2026-05-28 18:38:33 -0500

    Imprementación de vistas para alumnos cursos y notas

--- sistema_notas/academico/views.py
+++ sistema_notas/academico/views.py
@@ -1,3 +1,75 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Alumno, Curso, NotaAlumnoPorCurso
+from .forms import AlumnoForm, CursoForm, NotaAlumnoPorCursoForm
 
-# Create your views here.
+
+# ALUMNOS
+
+def crear_alumno(request):
+    if request.method == 'POST':
+        form = AlumnoForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('lista_alumnos')
+        
+    else:
+        form = AlumnoForm()
+
+    return render(request, 'academico/crear_alumno.html', {'form: form'})
+
+
+def lista_alumnos(request):
+    alumnos = Alumno.objects.all()
+
+    return render(request, 'academico/lista_alumnos.html', {
+        'alumnos': alumnos
+    })
+
+
+# CURSOS
+
+def crear_curso(request):
+    if request.method == 'POST':
+        form = CursoForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('lista_cursos')
+        
+    else:
+        form = CursoForm()
+
+    return render(request, 'academico/crear_curso.html', {'form': form})
+
+
+def lista_cursos(request):
+    cursos = Curso.objects.all()
+
+    return render(request, 'academico/lista_cursos.html', {
+        'cursos': cursos
+    })
+
+
+# NOTAS
+
+def crear_nota(request):
+    if request.method == 'POST':
+        form = NotaAlumnoPorCursoForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('lista_notas')
+        
+    else:
+        form = NotaAlumnoPorCursoForm()
+    
+    return render(request, 'academico/crear_nota.html', {'form': form})
+
+
+def lista_nitas(request):
+    notas = NotaAlumnoPorCurso.objetcs.all()
+
+    return render(request, 'academico/lista_notas.html', {
+        'notas': notas
+    })
